27ae0498d52bb474bd649ea730cbe4799ca444f9
Dave Koala <mr.d.clare@gmail.com>
2020-12-26 14:18:51 +0000

    add pwa settings

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  pwa: {
+    name: "My App",
+    themeColor: "#4DBA87",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      swSrc: "dev/sw.js"
+    }
+  }
 };
