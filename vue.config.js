f129669bd48ea9655f422098d7f12c2e2b507748
Dave Koala <mr.d.clare@gmail.com>
2020-12-26 14:21:05 +0000

    add more config

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,7 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
   pwa: {
-    name: "My App",
-    themeColor: "#4DBA87",
-    msTileColor: "#000000",
+    name: "skills-desire",
     appleMobileWebAppCapable: "yes",
     appleMobileWebAppStatusBarStyle: "black",
     workboxPluginMode: "InjectManifest",
