856deb77f9727dd9621d14e11cbe0d5fdddd0ab1
Dave Koala <mr.d.clare@gmail.com>
2021-02-01 10:38:42 +0000

    add charting

--- src/views/Home/SummaryCharts/SummaryCharts.vue
+++ src/views/Home/SummaryCharts/SummaryCharts.vue
@@ -1,15 +1,58 @@
 <template>
- <v-container>
-  some stuff here
-</v-container>
+  <div>
+    <horizontal-bar :data="chartData" :options="options" />
+  </div>
 </template>
-
 <script lang="ts">
 import Vue from "vue";
-export default Vue.extend({
-  name: "SummaryCharts" as string
-});
+import HorizontalBar from "@/views/Home/SummaryCharts/charts/HorizontalBar.vue";
 
+export default Vue.extend({
+  name: "SummaryCharts" as string,
+  components: { HorizontalBar },
+  computed: {
+    chartData(): unknown {
+      return {
+        labels: ["January", "February", "March"],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: "#f87979",
+            data: [40, 20, 30],
+          },
+          {
+            label: "Data 2",
+            backgroundColor: "#ffffff",
+            data: [30, 10, 35],
+          },
+        ],
+      };
+    },
+    options(): unknown {
+      return {
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: {
+          labels: {
+            fontColor: "white",
+            fontSize: 18,
+          },
+        },
+        scales: {
+          yAxes: [{ ticks: {fontColor: "white"} }],
+          xAxes: [
+            {
+              ticks: {
+                fontColor: "white",
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      };
+    },
+  },
+});
 </script>
 
 <style lang="scss" scoped></style>
---
+++ src/views/Home/SummaryCharts/charts/HorizontalBar.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import { HorizontalBar } from "vue-chartjs";
+
+export default {
+  name: "HorizontalBar" as string,
+  extends: HorizontalBar,
+
+  mounted(): void {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    this.renderChart(this.data, this.options);
+  },
+  props: {
+    data: { type: Object, required: true },
+    options: { type: Object, required: true },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
