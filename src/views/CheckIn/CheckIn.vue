66b2673e8370d11158a7d5adfc2c6d05dbb20214
Dave Koala <mr.d.clare@gmail.com>
2021-02-05 22:24:46 +0000

    transparent

--- src/views/CheckIn/CheckIn.vue
+++ src/views/CheckIn/CheckIn.vue
@@ -1,14 +1,14 @@
 <template>
   <v-container fluid>
-    <v-card :dark="$store.getters['App/dark']">
-      <v-list two-line>
+    <v-card :dark="$store.getters['App/dark']" color="transparent" tile flat>
+      <v-list two-line color="transparent">
         <v-subheader>Desires: Rate your ability.</v-subheader>
         <desire-check-in-list-item
           v-for="(desire, index) in desires"
           :key="`checkInItem-${index}`"
           :desire="desire"
         />
-
+        <v-divider></v-divider>
         <v-subheader>Skills: Rate your last practice.</v-subheader>
         <skill-check-in-list-item
           v-for="(skill, index) in skills"
