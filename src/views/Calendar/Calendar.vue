5187da3470a0ec514b6d28d2d5adb422d7e562c0
Dave Koala <mr.d.clare@gmail.com>
2021-03-04 17:23:26 +0000

    woo hooo its working

--- src/views/Calendar/Calendar.vue
+++ src/views/Calendar/Calendar.vue
@@ -3,50 +3,25 @@
     <h1>
       Nearly there a renderless component for creating 'add to calendar' links
     </h1>
-    <p>Need to fix outlook star and finish times</p>
-    <ICS :parameters="event">
-      <v-btn
-        slot-scope="{ Google }"
-        link
-        :href="Google"
-        color="blue-grey"
-        class="ma-2 white--text"
-        target="new"
-      >
-        <v-icon left dark> mdi-google </v-icon>
-        Google
-      </v-btn>
+
+    <ICS :parameters="event" v-for="os in calendarLinks" :key="os.name">
+      <template v-slot:default="slotProps">
+        <v-btn
+          link
+          :href="slotProps[os.name]"
+          color="blue-grey"
+          class="ma-2 white--text"
+          target="new"
+        >
+          <v-icon left dark> {{ os.icon }} </v-icon>
+          {{ os.name }}
+        </v-btn>
+      </template>
     </ICS>
 
-    <ICS :parameters="event">
-      <v-btn
-        slot-scope="{ Microsoft }"
-        link
-        :href="Microsoft"
-        color="blue-grey"
-        class="ma-2 white--text"
-        target="new"
-      >
-        <v-icon left dark> mdi-microsoft </v-icon>
-        Microsoft
-      </v-btn>
-    </ICS>
-    <ICS :parameters="event">
-      <v-btn
-        slot-scope="{ Office365 }"
-        link
-        :href="Office365"
-        color="blue-grey"
-        class="ma-2 white--text"
-        target="new"
-      >
-        <v-icon left dark> mdi-microsoft-office </v-icon>
-        Office365
-      </v-btn>
-    </ICS>
     <v-btn @click="download" class="ma-2 warning">
       <v-icon left dark>mdi-sort-calendar-ascending</v-icon>
-      Download ICS 
+      Download ICS
     </v-btn>
   </v-container>
 </template>
@@ -68,6 +43,13 @@ export default Vue.extend({
     text: "Hello world",
   }),
   computed: {
+    calendarLinks(): unknown {
+      return [
+        { name: "Google", icon: "mdi-google" },
+        { name: "Microsoft", icon: "mdi-microsoft" },
+        { name: "Office365", icon: "mdi-microsoft-office" },
+      ];
+    },
     event(): Parameters {
       const start = new Date("2021-03-14");
       const end = new Date("2021-03-14");
@@ -103,7 +85,7 @@ export default Vue.extend({
     createEvent(): string {
       const event = {
         title: "Lunch",
-        start: [2018, 1, 15, 12, 15],
+        start: [2021, 4, 15, 12, 15],
         duration: { minutes: 45 },
         // start: [2018, 5, 30, 6, 30],
         // duration: { hours: 6, minutes: 30 },
